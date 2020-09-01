f5df8fa9e87f4f19fefe047d24c10c64c18cca03
liu <2579186091>
2020-09-01 16:51:05 +0800

    css

--- home/js/conf.js
+++ home/js/conf.js
@@ -1,2 +1,2 @@
-// var url = "http://127.0.0.1:10087/api.php";
-var url = "https://admin.kxbwmedia.com:20005/api.php";
+var url = "http://127.0.0.1:10087/api.php";
+// var url = "https://admin.kxbwmedia.com:20005/api.php";
