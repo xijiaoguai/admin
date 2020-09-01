c43a33037f982bb8f0728ca5b82e562f8fd38233
liu <2579186091>
2020-09-01 17:21:35 +0800

    conf

--- home/js/conf.js
+++ home/js/conf.js
@@ -1,2 +1,9 @@
-// var url = "http://127.0.0.1:10087/api.php";
+var local = "http://127.0.0.1:10087/api.php";
 var url = "https://admin.kxbwmedia.com:20005/api.php";
+
+$.getJSON("js/.env", function (data) {
+    console.log(data['env']);
+    if (data['env'] != 'prod') {
+        url = local;
+    }
+})
